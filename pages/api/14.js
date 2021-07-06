dade003c0c09d6af39231953e272285cdd6bec39
FTZrecords <ftzrecords@gmail.com>
2021-07-07 07:44:26 +0900

    d

--- pages/api/14.js
+++ pages/api/14.js
@@ -1,17 +1,3 @@
-import fs from "fs";
-import path from "path";
-
-export default function FF(req, res) {
-  var min = 1;
-  var max = 15;
-  var ran = Math.floor(Math.random() * (max + 1 - min)) + min;
-
-  const filePath = path.resolve(process.cwd(), "img/14/" + ran + ".JPG");
-  const imageBuffer = fs.readFileSync(filePath);
-
-  console.log(filePath);
-
-  res.setHeader("Content-Type", "image/jpg");
-  res.statusCode = 200;
-  res.send(imageBuffer);
-}
+export default ({ query: { word } }, res) => {
+  res.status(200).json({ message: `you requested for ${word} ` });
+};
